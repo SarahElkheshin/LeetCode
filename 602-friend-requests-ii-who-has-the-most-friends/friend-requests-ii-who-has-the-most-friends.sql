48506fa6dbd4b6bfc46d852b2920566c582c06d9
Sarah Elkheshin <89637219+SarahElkheshin@users.noreply.github.com>
2024-04-20 10:35:54 +0200

    Time: 449 ms (99.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,7 @@
+WITH CTE AS (
+    SELECT requester_id AS id FROM RequestAccepted  
+    UNION ALL 
+    SELECT accepter_id AS id FROM RequestAccepted  
+)
+
+SELECT id, count(id) as num FROM CTE GROUP BY id ORDER BY num DESC LIMIT 1
