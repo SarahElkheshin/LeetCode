de91572b8b243567af9b3e80b7335b607c63614c
Sarah Elkheshin <89637219+SarahElkheshin@users.noreply.github.com>
2024-08-31 11:10:49 +0200

    Time: 3 ms (39.45%) | Memory: 8.5 MB (61.48%) - LeetSync

--- 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.cpp
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.cpp
@@ -2,24 +2,13 @@ class Solution {
 public:
     int minimumChairs(string s) {
         int chairs=0;
-        int empty=0;
+        int res=0;
         for(int i=0; s[i]; i++)
         {
-            if(s[i]=='E'&& empty==0)
-            {
-                chairs++;
-            }
-            else if(s[i]=='E' && empty>0 )
-            {
-                empty--;
-            }
-            else
-            {
-                empty++;
-
-            }
+            s[i]=='E'? chairs++:chairs--;
+            res = max(res,chairs);
         }
-        return chairs;
+        return res;
         
     }
 };
