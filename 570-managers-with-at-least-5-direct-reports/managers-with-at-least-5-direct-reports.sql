64242f8210abf501a72c51b6747d242a234da229
Sarah Elkheshin <89637219+SarahElkheshin@users.noreply.github.com>
2024-10-09 11:46:26 +0200

    Time: 352 ms (44.08%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,4 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-
-select name from employee inner join 
-(select managerid  FROM Employee  where managerid is not null group by managerid having count(id) >= 5) as m
-on employee.id = m.managerid
-
-
+# Write your MySQL query statement below
+SELECT name FROM  Employee E INNER JOIN
+(SELECT managerId FROM Employee GROUP BY managerId HAVING COUNT(managerId)>=5)TEMP
+ON E.id = TEMP.managerId
