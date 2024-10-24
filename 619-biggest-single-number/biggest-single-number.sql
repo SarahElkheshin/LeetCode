801e5487bcb38baa7a4537b62685726aa55a3a90
Sarah Elkheshin <89637219+SarahElkheshin@users.noreply.github.com>
2024-10-24 09:21:16 +0300

    Time: 437 ms (25.85%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,1 +1,9 @@
-SELECT MAX(num) as num FROM (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num) = 1 ) new
+# Write your MySQL query statement below
+SELECT IFNULL(
+    (SELECT num 
+     FROM MyNumbers 
+     GROUP BY num 
+     HAVING COUNT(num) = 1 
+     ORDER BY num DESC 
+     LIMIT 1), 
+    NULL) AS num;
