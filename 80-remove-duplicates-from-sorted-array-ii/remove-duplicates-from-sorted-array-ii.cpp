9feacae17b2aeb77894dc0bf2c0baf9152f8921d
Sarah Elkheshin <89637219+SarahElkheshin@users.noreply.github.com>
2024-12-11 22:16:58 +0200

    Time: 11 ms (6.80%) | Memory: 15 MB (5.38%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int k=0;
+        map<int,int> mp;
+        for (int i=0; i<nums.size(); i++)
+        {
+            if(mp[nums[i]]<2)
+            {
+                nums[k++]= nums[i];
+                mp[nums[i]]++;
+            }
+        }
+        return k;
+    }
+};
